fc653a2ac2095e2d280960075309e88fa7b813ef
Lien Hung Trinh <thuyhung2112@yahoo.com>
2025-11-30 21:51:20 +0700

    add prefix

--- app/lib/prefix.ts
+++ app/lib/prefix.ts
@@ -1,3 +1,5 @@
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '/xmas-decorate';
+// in local, replace with
+// const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export { prefix };
