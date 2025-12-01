adb0abcd6040bf1553ee4092a67c8ed3d0945c55
Lien Hung Trinh <thuyhung2112@yahoo.com>
2025-12-01 09:51:09 +0700

    fix path prefix

--- app/lib/prefix.ts
+++ app/lib/prefix.ts
@@ -1,4 +1,4 @@
-// Use empty string for local development, '/xmas-decorate' for production
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
+// Use empty string for local development
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '/xmas-decorate';
 
 export { prefix };
